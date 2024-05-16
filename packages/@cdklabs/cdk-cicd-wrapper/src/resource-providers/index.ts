1da025d3564e9cce3611adb066f4f44246c3a7b6
Milan Gyalai <gyalai@amazon.com>
2024-05-16 14:18:04 +0200

    fix: typo

--- packages/@cdklabs/cdk-cicd-wrapper/src/resource-providers/index.ts
+++ packages/@cdklabs/cdk-cicd-wrapper/src/resource-providers/index.ts
@@ -1,8 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import exp from 'constants';
-
 /**
  * Provider for creating a compliance bucket.
  */
