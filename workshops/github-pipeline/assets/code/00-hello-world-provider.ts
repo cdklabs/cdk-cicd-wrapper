d44181473d4be1a81b5cdc12a4fd31250ca0c1f4
Gezim Musliaj <gmuslia@amazon.de>
2024-11-05 17:14:24 +0100

    Deployed 97a8bd7 with MkDocs version: 1.6.0

--- workshops/github-pipeline/assets/code/00-hello-world-provider.ts
+++
@@ -1,8 +0,0 @@
-import * as wrapper from '@cdklabs/cdk-cicd-wrapper';
-import { HelloWorldStack } from '../lib/hello-world-stack';
-
-export class HelloWorldProvider extends wrapper.DefaultStackProvider {
-  stacks(): void {
-    new HelloWorldStack(this.scope, 'HelloWorldStack', { env: this.env });
-  }
-}
--- workshops/github-pipeline/assets/code/01-cdk-cicd-wrapper-github.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-
-const app = new cdk.App();
