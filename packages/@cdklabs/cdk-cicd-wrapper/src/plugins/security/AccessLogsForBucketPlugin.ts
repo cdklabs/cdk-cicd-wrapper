71a735113b36bc7c433ade8bcd19ffa319e9eb90
Gezim Musliaj <gmuslia@amazon.de>
2025-02-26 15:25:10 +0100

    fix: cross region deployment for compliance bucket

--- packages/@cdklabs/cdk-cicd-wrapper/src/plugins/security/AccessLogsForBucketPlugin.ts
+++ packages/@cdklabs/cdk-cicd-wrapper/src/plugins/security/AccessLogsForBucketPlugin.ts
@@ -1,9 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { IAspect, Aspects, Names } from 'aws-cdk-lib';
+import { IAspect, Aspects, Names, Stack } from 'aws-cdk-lib';
 import { CfnBucket } from 'aws-cdk-lib/aws-s3';
-import { Construct } from 'constructs';
+import { Construct, IConstruct } from 'constructs';
 import { GlobalResources, PluginBase, ResourceContext } from '../../common';
 
 /**
@@ -12,29 +12,63 @@ import { GlobalResources, PluginBase, ResourceContext } from '../../common';
 export class AccessLogsForBucketPlugin extends PluginBase {
   readonly name: string = 'AccessLogsForBucketPlugin';
 
-  readonly version: string = '1.0';
+  readonly version: string = '1.1';
 
   afterStage(scope: Construct, context: ResourceContext): void {
     const complianceLogBucketName = context.blueprintProps.deploymentDefinition[context.stage].complianceLogBucketName;
+    const region = context.blueprintProps.deploymentDefinition[context.stage].env.region;
 
     if (complianceLogBucketName) {
       // ensure it has been initialized if the plugin is active
       context.get(GlobalResources.COMPLIANCE_BUCKET);
-      Aspects.of(scope).add(new AccessLogsForBucketAspect(complianceLogBucketName));
+      Aspects.of(scope).add(new AccessLogsForBucketAspect(complianceLogBucketName, region));
     }
   }
 }
 
 class AccessLogsForBucketAspect implements IAspect {
-  constructor(private complianceLogBucketName: string) {}
+  constructor(
+    private complianceLogBucketName: string,
+    private mainRegion: string,
+  ) {}
 
   visit(node: Construct): void {
     if (node instanceof CfnBucket && this.complianceLogBucketName) {
-      // Configure S3 bucket logging
-      node.loggingConfiguration = {
-        destinationBucketName: this.complianceLogBucketName,
-        logFilePrefix: Names.uniqueId(node),
-      };
+      // Configure S3 bucket logging if none setup
+      // Only allow overwritting of logFilePrefix if setup
+      const stack = this.findStack(node);
+
+      if (!stack) {
+        throw new Error('Could not find stack for the bucket');
+      }
+
+      let complianceLogBucketName = this.complianceLogBucketName;
+
+      if (stack.region !== this.mainRegion) {
+        complianceLogBucketName = this.complianceLogBucketName.replace(this.mainRegion, stack.region);
+      }
+
+      if (node.loggingConfiguration === undefined) {
+        node.loggingConfiguration = {
+          destinationBucketName: complianceLogBucketName,
+          logFilePrefix: Names.uniqueId(node),
+        };
+      } else {
+        const currentLoggingConfig = node.loggingConfiguration as CfnBucket.LoggingConfigurationProperty;
+        if (currentLoggingConfig.logFilePrefix) {
+          node.loggingConfiguration = {
+            destinationBucketName: complianceLogBucketName,
+            logFilePrefix: currentLoggingConfig.logFilePrefix,
+          };
+        }
+      }
     }
+  }
+
+  findStack(node: IConstruct): Stack | undefined {
+    while (node && node.node.scope && !('stackName' in node)) {
+      node = node.node.scope;
+    }
+    return node as Stack;
   }
 }
